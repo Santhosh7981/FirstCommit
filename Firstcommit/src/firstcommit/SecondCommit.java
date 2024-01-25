1e08e637ac03c2b99885ed09b8a8ab7306869480
Santhosh7981 <s570722@nwmissouri.edu>
2024-01-25 16:43:42 -0600

    Second Git Commit

---
+++ Firstcommit/src/firstcommit/SecondCommit.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package firstcommit;
+
+/**
+ *
+ * @author S570722
+ */
+public class SecondCommit {
+    public static void main(String[] args) {
+        System.out.println("Hello World. I am coming for you!.");
+    }
+}
